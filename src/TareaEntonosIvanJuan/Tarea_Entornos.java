96bff3129b322f1f760b92427d42d451f87f8906
IvanTArroyo <ivanturroarroyo@gmail.com>
2024-01-09 12:56:28 +0100

    Cambio de cilindro

--- src/TareaEntonosIvanJuan/Tarea_Entornos.java
+++ src/TareaEntonosIvanJuan/Tarea_Entornos.java
@@ -6,10 +6,15 @@ public class Tarea_Entornos {
 	static Scanner entrada= new Scanner (System.in);
 
 	public static void main(String[] args) {
+		//declaracion de variables
 		int lado1=0;
 		int lado2=0;
+		int radio=0;
+		int altura=0;
 		double triangulo = areaTriangulo(lado1,lado2);
-		System.out.println("El área es: "+triangulo);
+		System.out.println("El área es del triangulo: "+triangulo);
+		double cilindro = areaCilindro(radio,altura);
+		System.out.println("El area del cilindro: "+cilindro);
 
 	}
 	public static double areaTriangulo (int lado1, int lado2) {
@@ -18,6 +23,14 @@ public class Tarea_Entornos {
 		System.out.println("introduzca el segundo lado");
 		lado2=entrada.nextInt();
 		double area=lado1*lado2;
+		return area;
+	}
+	public static double areaCilindro (int radio, int altura) {
+		System.out.println("Introduce el radio");
+		radio=entrada.nextInt();
+		System.out.println("Introduce la altura");
+		altura=entrada.nextInt();
+		double area=((2*Math.PI*radio*altura)+(2*Math.PI*radio*radio));
 		return area;
 	}
 
